7a3c3e30e524a521b1c4105bf76c77fa7e49508c
heidenbloed <git@heidenblut.eu>
2021-12-12 22:55:08 +0100

    Add RecipeCard and IconButton components.

---
+++ src/components/IconButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    class="w-12 h-12 rounded-lg"
+    :class="isPrimary ? 'bg-red-500 border-0' : 'bg-white border-2 border-red-500'"
+  >
+    <div
+      class="material-icons-outlined text-2xl"
+      :class="isPrimary ? 'text-white' : 'text-red-500'"
+    >
+      <slot></slot>
+    </div>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "primary",
+    validator: (val: string) => ["primary", "secondary"].includes(val),
+  }
+});
+const isPrimary = computed(() => props.type == "primary")
+</script>
---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="max-w-md bg-white rounded-xl shadow-xl">
+    <div class="rounded-xl overflow-hidden">
+      <img :src="imageUrl" alt="recipe image">
+    </div>
+    <div class="p-4 flex flex-row">
+      <div class="flex-grow pr-4">
+        <div class="font-sans font-semibold tracking-tight text-2xl text-gray-700">
+          <slot name=title></slot>
+        </div>
+        <div class="font-sans font-medium tracking-tight text-gray-700">
+          <slot name=description></slot>
+        </div>
+      </div>
+      <div class="flex flex-row gap-2">
+        <IconButton type="primary">
+          shopping_cart
+        </IconButton>
+        <IconButton type="secondary">
+          info
+        </IconButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconButton from "@/components/IconButton.vue";
+
+const props = defineProps<{
+  imageUrl: string
+}>();
+</script>
