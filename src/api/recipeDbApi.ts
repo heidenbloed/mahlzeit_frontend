633e1e9a8e1f6c156c64fc42972e720f3ee97315
heidenbloed <git@heidenblut.eu>
2022-03-09 21:43:58 +0100

    Add more api calls and move interfaces to seperate file.

--- src/api/recipeDbApi.ts
+++ src/api/recipeDbApi.ts
@@ -1,7 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+import {
+  IngredientCategory,
+  RecipeLabel,
+  Unit,
+  IngredientShort,
+  Ingredient,
+  IngredientEdit,
+  IngredientEditResponse,
+  RecipeShort,
+  RecipeData,
+  RecipeEdit,
+  RecipeEditResponse,
+  RecipeImageEdit,
+  RecipeImageEditResponse,
+} from "../types/recipeDbTypes";
 
 const recipeDbApi = axios.create({
-  baseURL: `http://0.0.0.0:8000/api`,
+  baseURL: `http://192.168.178.53:8000/api`,
   timeout: 30000,
 });
 
@@ -9,61 +24,139 @@ recipeDbApi.interceptors.response.use((response) => response, (error) => {
   console.error("RecipeDbApi error: " + error);
 });
 
-interface Model {
-  id: number;
+enum IngredientListOrdering {
+  nameAscending = "name",
+  nameDescending = "-name",
+  updatedAtAscending = "updated_at",
+  updatedAtDescending = "-updated_at",
 }
 
-interface UpdatedableModel extends Model {
-  updated_at: number;
+enum RecipeListOrdering {
+  nameAscending = "name",
+  nameDescending = "-name",
+  prepTimeAscending = "preparation_time",
+  prepTimeDescending = "-preparation_time",
+  updatedAtAscending = "updated_at",
+  updatedAtDescending = "-updated_at",
 }
 
-interface RecipeImageShort extends UpdatedableModel {
-  image: string;
+async function getIngredientCategoryList(): Promise<IngredientCategory[]> {
+  const response = await recipeDbApi.get("/ingredient_categories/");
+  return response.data;
 }
 
-interface RecipeImage extends RecipeImageShort {
-  order: number;
+async function getRecipeLabelList(search?: string): Promise<RecipeLabel[]> {
+  const params = {
+    search: search,
+  };
+  const response = await recipeDbApi.get("/labels/", {params: params});
+  return response.data;
 }
 
-interface RecipeLabel extends Model {
-  name: string;
-  category: string;
+async function getUnitList(): Promise<Unit[]> {
+  const response = await recipeDbApi.get("/units/");
+  return response.data;
 }
 
-interface IngredientShort extends UpdatedableModel {
-  name: string;
+async function getIngredientList(ordering: IngredientListOrdering, search?: string): Promise<IngredientShort[]>{
+  const params = {
+    ordering: ordering,
+    search: search,
+  };
+  const response = await recipeDbApi.get("/ingredients/", {params: params});
+  return response.data;
 }
 
-interface Unit extends Model {
-  short_form: string;
+async function getIngredientDetail(id: number): Promise<Ingredient> {
+  const response = await recipeDbApi.get("/ingredients/" + id + "/");
+  return response.data;
 }
 
-interface QuantifiedIngredient extends UpdatedableModel {
-  ingredient: IngredientShort;
-  quantity: number;
-  unit: Unit;
+async function createIngredient(ingredient: IngredientEdit): Promise<IngredientEditResponse> {
+  const response = await recipeDbApi.post("/ingredients/", ingredient);
+  return response.data;
 }
 
-interface RecipeShort extends UpdatedableModel {
-  name: string;
-  preparation_time: number;
-  first_image?: RecipeImageShort;
+async function updateIngredient(id: number, ingredient: Partial<IngredientEdit>): Promise<IngredientEditResponse> {
+  const response = await recipeDbApi.patch("/ingredients/" + id + "/", ingredient);
+  return response.data;
 }
 
-interface Recipe extends RecipeShort {
-  source: string;
-  num_servings: number;
-  labels: RecipeLabel[];
-  quantified_ingredients: QuantifiedIngredient[];
-  recipe_images: RecipeImage[];
+async function deleteIngredient(id: number) {
+  await recipeDbApi.delete("/ingredients/" + id + "/");
 }
 
-async function getRecipeList(): Promise<RecipeShort[]> {
-  return (await recipeDbApi.get("recipes/")).data;
+async function getRecipeList(ordering: RecipeListOrdering, search?: string, labels?: RecipeLabel[]): Promise<RecipeShort[]> {
+  const params = {
+    ordering: ordering,
+    search: search,
+    labels: labels?.map(label => label.id).join(","),
+  };
+  const response = await recipeDbApi.get("/recipes/", {params: params});
+  return response.data;
 }
 
-async function getRecipeDetail(id: number): Promise<Recipe> {
-  return (await recipeDbApi.get("recipes/" + id + "/")).data;
+async function getRecipeDetail(id: number): Promise<RecipeData> {
+  const response = await recipeDbApi.get("/recipes/" + id + "/");
+  return response.data;
 }
 
-export { getRecipeList, getRecipeDetail, RecipeShort, Recipe, RecipeImage }
+async function createRecipe(recipe: RecipeEdit): Promise<RecipeEditResponse> {
+  const response = await recipeDbApi.post("/recipes/", recipe);
+  return response.data;
+}
+
+async function updateRecipe(id: number, recipe: Partial<RecipeEdit>): Promise<RecipeEditResponse> {
+  const response = await recipeDbApi.patch("/recipes/" + id + "/", recipe);
+  return response.data;
+}
+
+async function deleteRecipe(id: number) {
+  await recipeDbApi.delete("/recipes/" + id + "/");
+}
+
+async function createRecipeImage(imageData: RecipeImageEdit): Promise<RecipeImageEditResponse> {
+  const formData = new FormData();
+  formData.append('image', imageData.image, `recipe_image_${imageData.recipe}.png`);
+  formData.append('order', imageData.order.toString());
+  formData.append('recipe', imageData.recipe.toString());
+  const response = await recipeDbApi.post("/recipe_image/", formData, {headers: {'Content-Type': 'multipart/form-data'}});
+  return response.data;
+}
+
+async function updateRecipeImage(id: number, imageData: Partial<RecipeImageEdit>): Promise<RecipeImageEditResponse> {
+  const formData = new FormData();
+  if (imageData.order) {
+    formData.append('order', imageData.order.toString());
+  }
+  if (imageData.recipe) {
+    formData.append('recipe', imageData.recipe.toString());
+  }
+  const response = await recipeDbApi.patch("/recipe_image/" + id + "/", formData, {headers: {'Content-Type': 'multipart/form-data'}});
+  return response.data;
+}
+
+async function deleteRecipeImage(id: number) {
+  await recipeDbApi.delete("/recipe_image/" + id + "/");
+}
+
+export {
+  getIngredientCategoryList,
+  getRecipeLabelList,
+  getUnitList,
+  getIngredientList,
+  getIngredientDetail,
+  createIngredient,
+  updateIngredient,
+  deleteIngredient,
+  getRecipeList,
+  getRecipeDetail,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+  createRecipeImage,
+  updateRecipeImage,
+  deleteRecipeImage,
+  RecipeListOrdering,
+  IngredientListOrdering,
+}
---
+++ src/types/recipeDbTypes.ts
@@ -0,0 +1,137 @@
+interface Model {
+  id: number;
+}
+
+interface UpdatedableModel extends Model {
+  updated_at: number;
+}
+
+interface RecipeImageShort extends UpdatedableModel {
+  image: string;
+}
+
+export interface RecipeImage extends RecipeImageShort {
+  order: number;
+}
+
+export interface RecipeImageEdit {
+  recipe: number;
+  image: Blob;
+  order: number;
+}
+
+export interface RecipeImageEditResponse extends RecipeImage {
+  recipe: number;
+}
+
+export interface RecipeLabel extends Model {
+  name: string;
+  category: string;
+}
+
+export interface IngredientShort extends UpdatedableModel {
+  name: string;
+}
+
+export interface Unit extends Model {
+  short_form: string;
+}
+
+export interface IngredientCategory extends Model {
+  name: string;
+  location_index: number;
+}
+
+export interface UnitConversion extends UpdatedableModel {
+  alternative_unit: Unit;
+  alternative_conversion_factor: number;
+  default_conversion_factor: number;
+}
+
+export interface Ingredient extends IngredientShort {
+  default_unit: Unit;
+  category: IngredientCategory;
+  unit_conversions: UnitConversion[];
+}
+
+export interface UnitConversionEdit {
+  alternative_unit: number;
+  alternative_conversion_factor: number;
+  default_conversion_factor: number;
+}
+
+export interface IngredientEdit {
+  name: string;
+  default_unit: number;
+  category: number;
+  unit_conversions: UnitConversionEdit[];
+}
+
+export interface IngredientEditResponse extends IngredientEdit, UpdatedableModel {};
+
+export interface QuantifiedIngredient extends UpdatedableModel {
+  ingredient: IngredientShort;
+  quantity: number;
+  unit: Unit;
+}
+
+export interface QuantifiedIngredientEdit {
+  ingredient: number;
+  quantity: number;
+  unit: number;
+}
+
+export interface RecipeShort extends UpdatedableModel {
+  name: string;
+  preparation_time: number;
+  first_image?: RecipeImageShort;
+}
+
+export interface RecipeData extends Omit<RecipeShort, "first_image"> {
+  source: string;
+  num_servings: number;
+  labels: RecipeLabel[];
+  quantified_ingredients: QuantifiedIngredient[];
+  recipe_images: RecipeImage[];
+}
+
+export interface RecipeEdit {
+  name: string;
+  preparation_time: number;
+  source: string;
+  num_servings: number;
+  labels: number[];
+  quantified_ingredients: QuantifiedIngredientEdit[];
+}
+
+export interface RecipeEditResponse extends RecipeEdit, UpdatedableModel {};
+
+export interface QuantifiedIngredientEditData {
+  id?: number;
+  ingredientId?: number;
+  ingredientName: string;
+  ingredientCategory: IngredientCategory;
+  quantity: number;
+  unit: Unit;
+  defaultUnit: Unit;
+  currentConversionFactor: number;
+  defaultConversionFactor: number;
+  setAsDefaultUnit: boolean;
+}
+
+export interface RecipeImageEditData {
+  id?: number;
+  image: string | Blob;
+  order: number;
+}
+
+export interface RecipeEditData {
+  id?: number;
+  name: string;
+  preparation_time: number;
+  source: string;
+  num_servings: number;
+  labels: RecipeLabel[];
+  quantified_ingredients: QuantifiedIngredientEditData[];
+  recipe_images: RecipeImageEditData[];
+}
