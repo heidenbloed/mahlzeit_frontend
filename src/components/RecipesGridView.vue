0ceae7d8cf08859208cbf4d93bd04c1bd9354118
heidenbloed <git@heidenblut.eu>
2022-06-16 23:48:46 +0200

    Use normal button instead of FAB on desktop.

--- src/components/RecipesGridView.vue
+++ src/components/RecipesGridView.vue
@@ -5,11 +5,21 @@
       v-model:labelFilterList="labelFilterList"
       name="recipeSearchBar"
     />
+    <div class="w-full hidden md:flex lg:col-span-2 xl:col-span-3 flex-row justify-center">
+      <RoundedButton type="primary" @click="$router.push('/recipe/new/')">
+        <template #default>
+          Neues Rezept
+        </template>
+        <template #icon>
+          add
+        </template>
+      </RoundedButton>
+    </div>
     <RecipeCard
       v-for="recipeCardData in recipeCardDataList"
       :recipeCardData="recipeCardData"
     />
-    <FloatingActionButton @click="$router.push('/recipe/new/')">
+    <FloatingActionButton class="block md:hidden" @click="$router.push('/recipe/new/')">
       add
     </FloatingActionButton>
   </CardGrid>
@@ -19,6 +29,7 @@
 import RecipeCard from "@/components/RecipeCard.vue";
 import CardGrid from "@/components/CardGrid.vue";
 import RecipeSearchAndFilterBar from "@/components/RecipeSearchAndFilterBar.vue";
+import RoundedButton from "@/components/RoundedButton.vue";
 import FloatingActionButton from "@/components/FloatingActionButton.vue";
 import { getRecipeList, RecipeListOrdering } from "../api/recipeDbApi";
 import { RecipeShort, RecipeLabel } from "../types/recipeDbTypes";
