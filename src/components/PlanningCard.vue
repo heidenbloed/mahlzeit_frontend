44f5fc301695a7c1713a051e5818b4422f16e073
heidenbloed <git@heidenblut.eu>
2022-06-22 23:43:20 +0200

    Fix image size of planning card. + Fix link to detail page of planning card.

--- src/components/PlanningCard.vue
+++ src/components/PlanningCard.vue
@@ -1,28 +1,26 @@
 <template>
-  <RoundedCard>
-    <div class="flex">
-      <button
-        @click="$router.push('/recipe/')"
-        class="w-1/4 overflow-hidden rounded-l-xl md:w-1/3"
-        type="button"
-      >
-        <img
-          :src="imageUrl"
-          alt="recipe image"
-          class="h-full object-cover object-center"
-        />
-      </button>
-      <div class="inline-block w-3/4 overflow-hidden p-2 md:w-2/3 md:p-4">
-        <h1 class="truncate text-xl font-semibold text-gray-700 md:text-2xl">
-          {{ recipeName }}
-        </h1>
-        <ServingsSlider v-model="_numServings" />
-        <div class="flex justify-end">
-          <RoundedButton type="flat" @click="removeFromPlannedRecipes">
-            <template v-slot:icon>{{ planningButtonIcon }}</template>
-            <template v-slot:default>{{ planningButtonText }}</template>
-          </RoundedButton>
-        </div>
+  <RoundedCard class="flex h-32 flex-row md:h-40">
+    <button
+      @click="$router.push(detailUrl)"
+      class="w-1/4 overflow-hidden rounded-l-xl md:w-1/3"
+      type="button"
+    >
+      <img
+        :src="imageUrl"
+        alt="recipe image"
+        class="h-full w-full object-cover object-center"
+      />
+    </button>
+    <div class="inline-block w-3/4 overflow-hidden p-2 md:w-2/3 md:p-4">
+      <h1 class="truncate text-xl font-semibold text-gray-700 md:text-2xl">
+        {{ recipeName }}
+      </h1>
+      <ServingsSlider v-model="_numServings" />
+      <div class="flex justify-end">
+        <RoundedButton type="flat" @click="removeFromPlannedRecipes">
+          <template v-slot:icon>{{ planningButtonIcon }}</template>
+          <template v-slot:default>{{ planningButtonText }}</template>
+        </RoundedButton>
       </div>
     </div>
   </RoundedCard>
@@ -33,6 +31,7 @@ import RoundedCard from "@/components/RoundedCard.vue";
 import ServingsSlider from "@/components/ServingsSlider.vue";
 import RoundedButton from "@/components/RoundedButton.vue";
 import { usePlannedRecipesStoreForRecipe } from "../stores/plannedRecipes";
+import { computed } from "vue";
 
 const props = defineProps<{
   recipeId: number;
@@ -47,4 +46,6 @@ const {
   planningButtonText,
   numServings: _numServings,
 } = usePlannedRecipesStoreForRecipe(props.recipeId);
+
+const detailUrl = computed(() => `/recipe/${props.recipeId}/`);
 </script>
