967ebfce3cc5fe55d296127c742c061d98b8361e
heidenbloed <git@heidenblut.eu>
2022-10-31 23:40:16 +0100

    Add loading spinner component.

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -4,29 +4,11 @@
       class="fixed inset-0 min-h-screen overflow-y-auto bg-black opacity-40"
     ></div>
     <div class="fixed inset-0 flex items-center justify-center">
-      <svg class="h-12 w-12 animate-spin" viewBox="0 0 100 100">
-        <circle
-          class="path"
-          cx="50"
-          cy="50"
-          r="45"
-          fill="none"
-          stroke="gray"
-          stroke-width="10"
-        ></circle>
-        <circle
-          class="path"
-          cx="50"
-          cy="50"
-          r="45"
-          fill="none"
-          stroke="white"
-          stroke-width="10"
-          stroke-linecap="round"
-          stroke-dashoffset="75"
-          stroke-dasharray="283"
-        ></circle>
-      </svg>
+      <LoadingSpinner />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import LoadingSpinner from "./LoadingSpinner.vue";
+</script>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg class="h-12 w-12 animate-spin" viewBox="0 0 100 100">
+    <circle
+      class="path"
+      cx="50"
+      cy="50"
+      r="45"
+      fill="none"
+      stroke="white"
+      stroke-width="10"
+      stroke-linecap="round"
+      stroke-dashoffset="75"
+      stroke-dasharray="283"
+    ></circle>
+  </svg>
+</template>
