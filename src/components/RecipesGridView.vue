5f0699a6af21ac1effa30a8553d440a12b8e6ff7
heidenbloed <git@heidenblut.eu>
2022-10-31 23:43:18 +0100

    Add infinity scrolling to recipe list.

--- src/components/RecipesGridView.vue
+++ src/components/RecipesGridView.vue
@@ -1,5 +1,5 @@
 <template>
-  <CardGrid>
+  <div class="flex h-full flex-col gap-2 p-2 md:gap-4">
     <RecipeSearchAndFilterBar
       v-model="searchString"
       v-model:labelFilterList="labelFilterList"
@@ -17,10 +17,23 @@
         <template #icon> add </template>
       </RoundedButton>
     </div>
-    <RecipeCard
-      v-for="recipeCardData in recipeCardDataList"
-      :recipeCardData="recipeCardData"
-    />
+
+    <main class="flex grow justify-center" ref="mainRef">
+      <ul
+        class="grid h-fit grid-cols-1 items-start justify-items-center gap-2 lg:grid-cols-2 lg:gap-6 xl:grid-cols-3"
+      >
+        <RecipeCard
+          v-for="recipeCardData in recipeCardDataList"
+          :recipeCardData="recipeCardData"
+        />
+        <li
+          class="col-span-1 flex w-full flex-row justify-center p-2 lg:col-span-2 xl:col-span-3"
+        >
+          <LoadingSpinner v-if="!allRecipesLoaded"></LoadingSpinner>
+        </li>
+      </ul>
+    </main>
+
     <FloatingActionButton
       class="block md:hidden"
       @click="$router.push('/recipe/new/')"
@@ -28,32 +41,81 @@
     >
       add
     </FloatingActionButton>
-  </CardGrid>
-  <div class="h-24 md:hidden"></div>
+    <div class="h-24 md:hidden"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import RecipeCard from "./RecipeCard.vue";
-import CardGrid from "./CardGrid.vue";
 import RecipeSearchAndFilterBar from "./RecipeSearchAndFilterBar.vue";
 import RoundedButton from "./RoundedButton.vue";
 import FloatingActionButton from "./FloatingActionButton.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 import { getRecipeList, RecipeListOrdering } from "../api/recipeDbApi";
 import { RecipeShort, RecipeLabel } from "../types/recipeDbTypes";
-import { ref, watchEffect } from "vue";
-import { useNetwork } from "@vueuse/core";
+import { ref, inject, Ref, watch } from "vue";
+import { useNetwork, useInfiniteScroll, useElementSize } from "@vueuse/core";
+import { computed } from "@vue/reactivity";
 
 const recipeCardDataList = ref<RecipeShort[]>([]);
-
 const searchString = ref("");
 const labelFilterList = ref<RecipeLabel[]>([]);
-watchEffect(async () => {
-  recipeCardDataList.value = await getRecipeList(
+const appDivRef = inject<Ref<HTMLDivElement | null>>("appDiv");
+const mainRef = ref<HTMLDivElement | null>();
+
+const currentListOffset = ref(0);
+const currentListCount = ref(-1);
+const allRecipesLoaded = computed(
+  () =>
+    currentListCount.value >= 0 &&
+    currentListOffset.value >= currentListCount.value
+);
+const { width: mainWidth, height: mainHeight } = useElementSize(mainRef);
+const initialChunkLength = computed(
+  () => Math.ceil((mainWidth.value / 400) * (mainHeight.value / 200)) + 3
+);
+const additionalChunkLength = 6;
+let initialChunkLoaded = false;
+
+async function loadRecipeList(initialLoad: boolean) {
+  let chunkLength = additionalChunkLength;
+  if (initialLoad) {
+    currentListOffset.value = 0;
+    chunkLength = initialChunkLength.value;
+  }
+  const recipeListChunk = await getRecipeList(
     RecipeListOrdering.updatedAtDescending,
     searchString.value,
-    labelFilterList.value
+    labelFilterList.value,
+    chunkLength,
+    currentListOffset.value
   );
+  if (initialLoad) {
+    recipeCardDataList.value = recipeListChunk.results;
+  } else {
+    recipeCardDataList.value.push(...recipeListChunk.results);
+  }
+  currentListOffset.value += chunkLength;
+  currentListCount.value = recipeListChunk.count;
+}
+watch(mainHeight, () => {
+  if (mainHeight.value > 0 && !initialChunkLoaded) {
+    loadRecipeList(true);
+    initialChunkLoaded = true;
+  }
 });
+watch([searchString, labelFilterList], () => loadRecipeList(true));
+
+useInfiniteScroll(
+  appDivRef,
+  async () => {
+    console.log("onLoadMoreData");
+    if (!allRecipesLoaded.value) {
+      loadRecipeList(false);
+    }
+  },
+  { distance: 100 }
+);
 
 const { isOnline } = useNetwork();
 </script>
